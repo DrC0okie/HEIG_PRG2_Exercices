385fed904fafdf51b88da70c075986e51d082caf
DrCookie <timothee.vanhove@heig-vd.ch>
2022-03-14 15:21:38 +0100

    Added 1.8

---
+++ Chap_1_Introduction/1.8/cmake-build-debug/CMakeFiles/Exercices.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercices.dir/main.c.obj"
+  "CMakeFiles/Exercices.dir/main.c.obj.d"
+  "Exercices.exe"
+  "Exercices.exe.manifest"
+  "Exercices.pdb"
+  "libExercices.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chap_1_Introduction/1.8/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+//Sans faire usage de l'instruction if et en utilisant exclusivement des opérateurs de
+//manipulation de bits, implémenter la fonction dont le prototype et la sémantique sont donnés
+
+void printArray(int8_t binary[], int size)
+{
+   for (int i = size -1 ; i >= 0; --i)
+   {
+      printf("%d", binary[i]);
+      printf("%s", " ");
+   }
+}
+
+void decimalToBinary(int32_t n, int8_t binary[]) {
+   int32_t loop = sizeof(int32_t)*8;
+   for (int i = 0; i < loop; ++i)
+   {
+      binary[i] = (int8_t)(n & 0x1);
+      n = n >> 1;
+   }
+   printArray(binary, loop);
+}
+
+int main(void) {
+   int n = 0x8;
+   int8_t binaryArray[sizeof(int32_t)*4];
+   decimalToBinary(8, binaryArray);
+   return EXIT_SUCCESS;
+}
