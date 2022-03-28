96a4f825b6ea51494e379ab69109ae565f4a347e
DrCookie <timothee.vanhove@heig-vd.ch>
2022-03-28 15:16:53 +0200

    Added 2.2

--- Chap_2_Pointeurs/2.2/main.c
+++ Chap_2_Pointeurs/2.2/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void squareCube(double* square, double* cube);
 int main() {
-   printf("Hello, World!\n");
-   return 0;
+   double cube = 2;
+   double square = 3;
+   double* ptrCube = &cube, *ptrSquare = &square;
+   squareCube(ptrSquare, ptrCube);
+   printf("Square of 3 is: %.f, cube of 2 is: %.f", square, cube);
 }
+void squareCube(double* square, double* cube)
+{
+   *square *= *square;
+   *cube = *cube * *cube * *cube;
+}
+
